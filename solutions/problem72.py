ec993561a6608a7d94c113aa57b2bec3c92b2855
TJ Combs <48781234+tjcombs@users.noreply.github.com>
2020-10-20 23:09:05 -1000

    Solved problem 72

---
+++ solutions/problem72.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 22:56:35 2020
+
+@author: tjcombs
+
+"""
+
+import numpy as np
+import pandas as pd
+from sympy import factorint
+
+def phi(x):
+    '''
+    Euler's totient function
+    Returns the number of numbers 1<=y<x such that y and x are reletively prime.
+    The fraction y/x with 1<=y<x is a proper fracton if and only if y and x 
+    are reletively prime.
+    '''
+    factors = factorint(x)
+    components = [p**factors[p] - p**(factors[p]-1) for p in factors] + [1]
+    return np.product(components)
+
+data = pd.DataFrame({'d': range(2, 1000001)})
+data['PROPER_FRACTIONS'] = data['d'].apply(phi)
+answer = data['PROPER_FRACTIONS'].sum()
+assert(answer == 303963552391)
